557b90fe23f866b63260d6fdc84f1147a4c6bc52
Jose Morado <jose.rm003@gmail.com>
2020-09-09 19:34:37 -0700

    Added login screen.

---
+++ app/src/main/java/com/example/gradetracker_teamk/LoginPage.java
@@ -0,0 +1,85 @@
+package com.example.gradetracker_teamk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginPage extends AppCompatActivity {
+    private Button log_button;
+
+    private EditText username;
+    private EditText password;
+
+
+    class User {
+        private String user;
+        private String pass;
+
+        public User(String user, String pass) {
+            this.user = user;
+            this.pass = pass;
+        }
+        public String getUser() {
+            return user;
+        }
+        public void setUser(String user) {
+            this.user = user;
+        }
+        public String getPass() {
+            return pass;
+        }
+        public void setPass(String pass) {
+            this.pass = pass;
+        }
+    }
+    public static List<User> users = new ArrayList<>();
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        users.add(new User("userone", "passone"));
+        users.add(new User("usertwo", "passtwo"));
+        users.add(new User("userthree", "passthree"));
+
+        log_button = findViewById(R.id.home_button);
+        username = findViewById(R.id.home_text_view_2);
+        password = findViewById(R.id.home_text_view_3);
+
+        log_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    Intent intent = new Intent(LoginPage.this, MainActivity.class);
+                    startActivity(intent);
+            }
+        });
+    }
+
+    boolean check_fields(EditText s1, EditText s2){
+        String str1 = s1.getText().toString();
+        String str2 = s2.getText().toString();
+        for(User i:users){
+            if (i.getUser().equals(str1)){
+                if(i.getPass().equals(str2)){
+                    return true;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+    public static Intent getintent(Context context){
+        Intent intent = new Intent(context, LoginPage.class);
+        return intent;
+    }
+}
--- app/src/main/java/com/example/gradetracker_teamk/MainActivity.java
+++ app/src/main/java/com/example/gradetracker_teamk/MainActivity.java
@@ -6,19 +6,35 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.gradetracker_teamk.db.AppDataBase;
 import com.example.gradetracker_teamk.db.UsersDAO;
 
 public class MainActivity extends AppCompatActivity {
+    private Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         AppDataBase.getInstance(getApplicationContext());
+        b1 = findViewById(R.id.login_button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginPage.getintent(getApplicationContext()));
+                startActivity(intent);
+            }
+        });
 
     }
 
