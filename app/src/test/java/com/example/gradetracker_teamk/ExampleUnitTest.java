5a1e64a171998f12f184aaff7adef3ba8e3ff4f4
HotakBawar <54864313+HotakBawar@users.noreply.github.com>
2020-09-18 19:54:27 -0700

    added unit test case for assignment

--- app/src/test/java/com/example/gradetracker_teamk/ExampleUnitTest.java
+++ app/src/test/java/com/example/gradetracker_teamk/ExampleUnitTest.java
@@ -2,6 +2,7 @@ package com.example.gradetracker_teamk;
 
 import android.content.Context;
 
+import com.example.gradetracker_teamk.Model.Assignment;
 import com.example.gradetracker_teamk.Model.Course;
 import com.example.gradetracker_teamk.Model.User;
 
@@ -67,6 +68,30 @@ public class ExampleUnitTest {
 
     }
 
+    @Test
+    public void assignment_tests() {
+        Assignment assignment = new Assignment("Cst438Hw1", 10, 9.0, "BinaryToDecimal", "09/20/20", 111);
+
+        assertEquals("Cst438Hw1", assignment.getAssignment());
+
+        assignment.setAssignment("Cst438Hw2");
+        assertEquals("Cst438Hw2", assignment.getAssignment());
+
+        assignment.setMaxScore(20);
+        assertEquals(20, assignment.getMaxScore());
+
+        assignment.setEarnedScore(19.0);
+        assertEquals(19.0, assignment.getEarnedScore(), 0.1);
+
+        assignment.setDescription("IntToBinary");
+        assertEquals("IntToBinary", assignment.getDescription());
+
+        assignment.setDateDue("09/25/20");
+        assertEquals("09/25/20", assignment.getDateDue());
+
+        assignment.setCourseId(123);
+        assertEquals(123, assignment.getCourseId());
 
 
+    }
 }
